d930e294e6704f1ccdff88a79b5b5165167ceb56
Tom Chen <developer@tomchentw.com>
2015-09-11 11:07:12 +0800

    chore(package.json): setup basic environment
    * npm run serve

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var reactify = require('reactify');
+var server = require('gulp-server-livereload');
 
 gulp.task('default', function() {
   var bundler = watchify(browserify({
@@ -26,4 +27,14 @@ gulp.task('default', function() {
   };
   build();
   bundler.on('update', build);
+
+  gulp.src('./client')
+    .pipe(server({
+      livereload: {
+        enable: true,
+        filter: function(filePath, cb) {
+          cb( /main.js/.test(filePath) );
+        }
+      },
+    }));
 });
